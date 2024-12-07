ea1260ac6f708dcb3486ee74e40ba9b8011c67b6
hetbhalani <bhalanihet2006@gmail.com>
2024-12-07 15:19:13 +0530

    mean

--- main.py
+++ main.py
@@ -33,13 +33,18 @@ def main():
         total_sum = sum(l2)
         mean = sum(fixi)/total_sum
         
-        print("|  xi  |  fi  | fixi |")
-        print("|------|------|------|")
+        
+        print("| xi  |  fi | fixi|")
+        print("|-----|-----|-----|")
         for i in range (0,n):
             
             print(f'|  {l1[i]}  |  {l2[i]}  |  {fixi[i]}  |')
-            print("|------|------|------|")
-
+            print("|-----|-----|-----|")
+        print(f'|  {total_sum}  |     |  {sum(fixi)  }')
+        
+    elif(a1 == 2):
+        n = int(input("Enter number of data: "))
+        
             
     print(f'mean of the sum is {mean}')
 
