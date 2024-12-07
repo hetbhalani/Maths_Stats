f5df86ed0dc526c60ff5539aaa822777c58625d1
hetbhalani <bhalanihet2006@gmail.com>
2024-12-07 15:38:28 +0530

    mean

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ def main():
     fixi = []
     total_sum = 0
     mean = 0
+    temp = []
     a1 = int(input("enter 1 for normal and 2 for rang data: "))
     
     if a1 == 1:
@@ -46,16 +47,30 @@ def main():
     elif(a1 == 2):
         n = int(input("Enter number of data: "))
         for i in range (0,n):
-            temp = (input(f"Enter a {i+1} range: ").split("-"))
-            xi.append((temp[0]+temp[1])/2)    
-            temp.clear() 
-            fi1 = int(input(f'Enter frenquency of {i+1}: '))
-            fi.append(fi1[i])
+            temp.append((input(f"Enter a {i+1} range: ")))
+            tempOfTemp = temp[i].split("-")
+            xi.append((int(tempOfTemp[0])+int(tempOfTemp[1]))/2)    
+            tempOfTemp.clear() 
+            fi.append(int(input(f'Enter frenquency of {i+1}: ')))
             
         for i in range (0,n):
             fixi.append(xi[i] * fi[i])
             
+        total_sum = sum(fi)
+        mean = sum(fixi)/total_sum
+        
+        print("| xi  |  fi | fixi|")
+        print("|-----|-----|-----|")
+        for i in range (0,n):
             
+            print(f'|  {temp[i]}  |  {fi[i]}  |  {fixi[i]}  |')
+            print("|-----|-----|-----|")
+        print(f'|    |   {total_sum}  |  {sum(fixi)  }')
+        
+    else:
+        print("Enter a valid choice bhaibandh")
+            
+    print()
     print(f'mean of the sum is {mean}')
 
 if __name__ == "__main__":
