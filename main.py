ce96a1a34d3d9f7964cf0a38eebf848d5ae0b00b
hetbhalani <bhalanihet2006@gmail.com>
2024-12-07 16:31:38 +0530

    mean

--- main.py
+++ main.py
@@ -53,3 +53,81 @@ def main():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+# from flask import Flask, render_template, request
+
+# app = Flask(__name__)
+
+# @app.route("/", methods=["GET", "POST"])
+# def main():
+#     xi = []
+#     fi = []
+#     fixi = []
+#     total_sum = 0
+#     mean = 0
+#     temp = []
+    
+#     if request.method == "POST":
+#         a1 = int(input("enter 1 for normal and 2 for rang data: "))
+        
+#         if a1 == 1:
+#             n = int(input("Enter number of data: "))
+#             for i in range (0,n):
+#                 xi1 = int(input(f'Enter {i+1} data: '))
+#                 fi1 = int(input(f'Enter frenquency of {i+1}: '))
+#                 xi.append(xi1)
+#                 fi.append(fi1)
+            
+#             for i in range (0,n):
+#                 fixi.append(xi[i] * fi[i])
+                
+#             total_sum = sum(fi)
+#             mean = sum(fixi)/total_sum
+            
+            
+#             print("| xi  |  fi | fixi|")
+#             print("|-----|-----|-----|")
+#             for i in range (0,n):
+                
+#                 print(f'|  {xi[i]}  |  {fi[i]}  |  {fixi[i]}  |')
+#                 print("|-----|-----|-----|")
+#             print(f'|  {total_sum}  |     |  {sum(fixi)  }')
+            
+            
+#         elif(a1 == 2):
+#             n = int(input("Enter number of data: "))
+#             for i in range (0,n):
+#                 temp.append((input(f"Enter a {i+1} range: ")))
+#                 tempOfTemp = temp[i].split("-")
+#                 xi.append((int(tempOfTemp[0])+int(tempOfTemp[1]))/2)    
+#                 tempOfTemp.clear() 
+#                 fi.append(int(input(f'Enter frenquency of {i+1}: ')))
+                
+#             for i in range (0,n):
+#                 fixi.append(xi[i] * fi[i])
+                
+#             total_sum = sum(fi)
+#             mean = sum(fixi)/total_sum
+            
+#             print("| xi  |  fi | fixi|")
+#             print("|-----|-----|-----|")
+#             for i in range (0,n):
+                
+#                 print(f'|  {temp[i]}  |  {fi[i]}  |  {fixi[i]}  |')
+#                 print("|-----|-----|-----|")
+#             print(f'|    |   {total_sum}  |  {sum(fixi)  }')
+            
+#         else:
+#             print("Enter a valid choice bhaibandh")
+                
+#             print(f'mean of the sum is {mean}') 
+#         return render_template("home.html", xi=xi, fi=fi, fixi=fixi, total_sum=total_sum, mean=mean, n=n)
+
+#     return render_template("home.html")
+
+
+# if __name__ == "__main__":
+#     main()
+    
+    
